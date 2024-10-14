272f421e01dcea6df807d84902ed90d53abfa57d
ZiederZiet <ziederziet@gmail.com>
2024-10-14 14:07:42 +0200

    Brain Mob Target Fix

--- src/main/java/io/github/ziederziet/beundead/mixin/PlayerSensorMixin.java
+++ src/main/java/io/github/ziederziet/beundead/mixin/PlayerSensorMixin.java
@@ -2,20 +2,52 @@ package io.github.ziederziet.beundead.mixin;
 
 import io.github.ziederziet.beundead.BeUndead;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntitySelector;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.ai.Brain;
+import net.minecraft.world.entity.ai.memory.MemoryModuleType;
 import net.minecraft.world.entity.ai.sensing.PlayerSensor;
+import net.minecraft.world.entity.ai.sensing.Sensor;
 import net.minecraft.world.entity.player.Player;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import java.util.Comparator;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 @Mixin(PlayerSensor.class)
-public class PlayerSensorMixin {
-    @Inject(at = @At("HEAD"), method = "doTick(Lnet/minecraft/server/level/ServerLevel;Lnet/minecraft/world/entity/LivingEntity;)V", cancellable = true)
-    protected void doTick(ServerLevel pLevel, LivingEntity pEntity, CallbackInfo info){
-        if (pEntity instanceof Player player && BeUndead.getZombieType(player) > 0){
-            info.cancel();
-        }
+public abstract class PlayerSensorMixin extends Sensor<LivingEntity> {
+//    @Inject(at = @At("HEAD"), method = "doTick(Lnet/minecraft/server/level/ServerLevel;Lnet/minecraft/world/entity/LivingEntity;)V", cancellable = true)
+//    protected void doTick(ServerLevel pLevel, LivingEntity pEntity, CallbackInfo info){
+//        if (pEntity instanceof Player player && BeUndead.getZombieType(player) > 0){
+//            info.cancel();
+//        }
+//    }
+
+    @Overwrite
+    protected void doTick(ServerLevel pLevel, LivingEntity pEntity) {
+        Stream var10000 = pLevel.players().stream().filter(EntitySelector.NO_SPECTATORS).filter((p_26744_) -> {
+            return pEntity.closerThan(p_26744_, 16.0) && p_26744_ instanceof Player player && BeUndead.getZombieType(player) > 0;
+        });
+        Objects.requireNonNull(pEntity);
+        List<Player> $$2 = (List)var10000.sorted(Comparator.comparingDouble(entity -> pEntity.distanceToSqr((Entity) entity))).collect(Collectors.toList());
+        Brain<?> $$3 = pEntity.getBrain();
+        $$3.setMemory(MemoryModuleType.NEAREST_PLAYERS, $$2);
+        List<Player> $$4 = (List)$$2.stream().filter((p_26747_) -> {
+            return isEntityTargetable(pEntity, p_26747_);
+        }).collect(Collectors.toList());
+        $$3.setMemory(MemoryModuleType.NEAREST_VISIBLE_PLAYER, $$4.isEmpty() ? null : (Player)$$4.get(0));
+        Optional<Player> $$5 = $$4.stream().filter((p_148304_) -> {
+            return isEntityAttackable(pEntity, p_148304_);
+        }).findFirst();
+        $$3.setMemory(MemoryModuleType.NEAREST_VISIBLE_ATTACKABLE_PLAYER, $$5);
     }
 }
---
+++ src/main/java/io/github/ziederziet/beundead/mixin/SensorMixin.java
@@ -0,0 +1,32 @@
+package io.github.ziederziet.beundead.mixin;
+
+import io.github.ziederziet.beundead.BeUndead;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.ai.memory.MemoryModuleType;
+import net.minecraft.world.entity.ai.sensing.Sensor;
+import net.minecraft.world.entity.player.Player;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.UUID;
+
+@Mixin(Sensor.class)
+public class SensorMixin {
+    @Inject(at = @At("RETURN"), method = "isEntityTargetable(Lnet/minecraft/world/entity/LivingEntity;Lnet/minecraft/world/entity/LivingEntity;)Z", cancellable = true)
+    private static boolean isEntityTargetable(LivingEntity pLivingEntity, LivingEntity pTarget, CallbackInfoReturnable<Boolean> info){
+        if (pLivingEntity.getBrain().hasMemoryValue(MemoryModuleType.ANGRY_AT)){
+            UUID uuid = pLivingEntity.getBrain().getMemory(MemoryModuleType.ANGRY_AT).get();
+            if (uuid.getMostSignificantBits() == pTarget.getUUID().getMostSignificantBits() &&
+                    uuid.getLeastSignificantBits() == pTarget.getUUID().getLeastSignificantBits()){
+                info.setReturnValue(true);
+                return true;
+            }
+        }
+        if (pTarget instanceof Player player && BeUndead.getZombieType(player) > 0){
+            info.setReturnValue(!info.getReturnValue());
+        }
+        return false;
+    }
+}
