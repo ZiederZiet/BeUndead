a83e9f4eef89e59b3081599e41cf31034785342c
ZiederZiet <ziederziet@gmail.com>
2025-02-22 12:42:40 +0100

    Revert "Fixed crash bug when killed by mob"
    This reverts commit 24212e88d1acc0251c79c2b09a412c9c81ea46a8.

--- src/main/java/io/github/ziederziet/beundead/event/ModEvents.java
+++ src/main/java/io/github/ziederziet/beundead/event/ModEvents.java
@@ -440,7 +440,7 @@ public class ModEvents {
                                     }
                                 }
                             }
-                            if (mob.getLastHurtByMob() != null && mob.getLastHurtByMob().is(player)){
+                            if (mob.getLastHurtByMob() != null && mob.getTarget().is(player)){
                                 mob.setLastHurtByMob(null);
                             }
                         });
