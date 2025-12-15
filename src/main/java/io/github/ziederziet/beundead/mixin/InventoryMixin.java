0cbd22c4de3f2a9e16ddc6b25720bea7f02bf623
ZiederZiet <ziederziet@gmail.com>
2025-12-15 16:37:28 +0100

    0.9.8

--- src/main/java/io/github/ziederziet/beundead/mixin/InventoryMixin.java
+++ src/main/java/io/github/ziederziet/beundead/mixin/InventoryMixin.java
@@ -75,9 +75,6 @@ public abstract class InventoryMixin {
                     info.setReturnValue(false);
                 }
             }
-            else {
-                info.setReturnValue(false);
-            }
         }
     }
 
