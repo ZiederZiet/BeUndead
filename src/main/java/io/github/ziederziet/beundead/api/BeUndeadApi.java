e8ba2c3c10666185ca1665230f954c32228f2c47
ZiederZiet <ziederziet@gmail.com>
2026-01-10 11:49:19 +0100

    0.9.12 Hot Fix

--- src/main/java/io/github/ziederziet/beundead/api/BeUndeadApi.java
+++ src/main/java/io/github/ziederziet/beundead/api/BeUndeadApi.java
@@ -1,8 +1,11 @@
 package io.github.ziederziet.beundead.api;
 
+import io.github.ziederziet.beundead.common.BeUndeadHelper;
 import net.fabricmc.fabric.api.event.Event;
 import net.fabricmc.fabric.api.event.EventFactory;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.player.Player;
+import org.jetbrains.annotations.Nullable;
 
 public class BeUndeadApi {
     public static final Event<OnPlayerTurnedUndead> PLAYER_TURNED_UNDEAD_EVENT = EventFactory.createArrayBacked(OnPlayerTurnedUndead.class,
@@ -27,5 +30,21 @@ public class BeUndeadApi {
     @FunctionalInterface
     public interface OnPlayerRevived {
         void onPlayerRevived(ServerPlayer player);
+    }
+
+    public static void startConverting(Player player, @Nullable Player converter){
+        BeUndeadHelper.startConverting(player, converter);
+    }
+
+    public static void revive(Player player){
+        BeUndeadHelper.revive(player, false);
+    }
+
+    public static void setUndeadType(Player player, String string){
+        BeUndeadHelper.setUndeadType(player, string, true);
+    }
+
+    public static String getUndeadType(Player player){
+        return BeUndeadHelper.getUndeadTypeName(player);
     }
 }
